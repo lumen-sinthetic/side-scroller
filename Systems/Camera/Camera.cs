323f1a04dfa20b1b83c8df212cc9196108aef696
lumen-sinthetic <vadimsi2105@gmail.com>
2026-02-28 11:16:31 +0500

    live camera and screen side added

--- Systems/Camera/Camera.cs
+++ Systems/Camera/Camera.cs
@@ -3,6 +3,9 @@ using System;
 
 public partial class Camera : Camera2D
 {
+
+	[Export]
+	public Node2D Target = null!;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
@@ -11,5 +14,6 @@ public partial class Camera : Camera2D
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		Position = Target.Position;
 	}
 }
